9fb70a82295e4d080d48610d02c0b71745aec921
marunfaiv <owoowo700@gmail.com>
2022-03-12 19:19:03 +0700

    aaa

--- web/src/index.js
+++ web/src/index.js
@@ -4,35 +4,40 @@ import cv from "opencv.js";
 let video = document.getElementById("videoInput"); // video is the id of video tag
 video.width = 1280;
 video.height = 720;
-navigator.mediaDevices
-  .getUserMedia({ video: true, audio: false })
-  .then(function(stream) {
-    video.srcObject = stream;
-    video.play();
 
-    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-    let cap = new cv.VideoCapture(video);
+function startVideo(){
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(function(stream) {
+      video.srcObject = stream;
+      video.play();
 
-    const FPS = 30;
-    function processVideo() {
-      try {
-        let begin = Date.now();
-        // start processing.
-        cap.read(src);
-        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-        cv.imshow("canvasOutput", dst);
-        // schedule the next one.
-        let delay = 1000 / FPS - (Date.now() - begin);
-        setTimeout(processVideo, delay);
-      } catch (err) {
-        console.error(err);
+      let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+      let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+      let cap = new cv.VideoCapture(video);
+
+      const FPS = 30;
+      function processVideo() {
+        try {
+          let begin = Date.now();
+          // start processing.
+          cap.read(src);
+          cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+          cv.imshow("canvasOutput", dst);
+          // schedule the next one.
+          let delay = 1000 / FPS - (Date.now() - begin);
+          setTimeout(processVideo, delay);
+        } catch (err) {
+          console.error(err);
+        }
       }
-    }
 
-    // schedule the first one.
-    setTimeout(processVideo, 0);
-  })
-  .catch(function(err) {
-    console.log("An error occurred! " + err);
-  });
+      // schedule the first one.
+      setTimeout(processVideo, 0);
+    })
+    .catch(function(err) {
+      console.log("An error occurred! " + err);
+    });
+}
+
+startVideo()
